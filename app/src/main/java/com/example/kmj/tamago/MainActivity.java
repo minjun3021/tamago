97ddadbd793521d54d2bf2a75c55612a71ef06ef
minjun3021 <minjun3021@naver.com>
2018-05-13 03:00:08 +0900

    안스내생애첫앱

---
+++ app/src/main/java/com/example/kmj/tamago/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.kmj.tamago;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView text;
+    ImageView image;
+    Button button;
+    int i = 200;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text=(TextView) findViewById(R.id.mainText);
+        image=(ImageView) findViewById(R.id.mainImage);
+        button=(Button) findViewById(R.id.mainButton);
+
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //count i--;
+                if(i<0) {
+                    Toast.makeText(getApplicationContext(), "다끝남ㅇㅇ", Toast.LENGTH_SHORT).show();
+                    i = 0;
+                }
+               i--;
+               text.setText(String.valueOf(i));
+
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i=200;
+                text.setText(String.valueOf(i));
+                //리셋
+            }
+        });
+
+    }
+}
+
+//#1
+//클릭
+//클릭횟수
+//리셋
+
+//#2
+//알깨지는거
+//100img
+//50img
+//30img
+//10img
